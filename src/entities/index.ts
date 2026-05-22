db8b357cbbf7f2522d0ee50f253c1f43ffcd4fb5
Wix Remote Machine <support@wix.com>
2026-05-22 19:25:50 +0000

    Initial Generation

---
+++ src/entities/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Auto-generated entity types
+ * Contains all CMS collection interfaces in a single file 
+ */
+
+/**
+ * Collection ID: mostwantedlist
+ * Interface for MostWantedList
+ */
+export interface MostWantedList {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType number */
+  rank?: number;
+  /** @wixFieldType text */
+  personName?: string;
+  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
+  personImage?: string;
+  /** @wixFieldType text */
+  description?: string;
+  /** @wixFieldType text */
+  lastKnownLocation?: string;
+}
