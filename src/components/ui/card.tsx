02b50e93d19ff6654f75475c8f0183151a02cb6d
Wix Remote Machine <support@wix.com>
2026-05-23 22:37:23 +0000

    Manual code changes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -41,7 +41,7 @@ const CardTitle = React.forwardRef<
 ))
 CardTitle.displayName = "CardTitle"
 
-const CardDescription = React.forwardRef<
+const CardDescription1 = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
@@ -51,7 +51,7 @@ const CardDescription = React.forwardRef<
     {...props}
   />
 ))
-CardDescription.displayName = "CardDescription"
+CardDescription1.displayName = "CardDescription1"
 
 const CardContent = React.forwardRef<
   HTMLDivElement,
@@ -73,4 +73,4 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription1, CardContent }
