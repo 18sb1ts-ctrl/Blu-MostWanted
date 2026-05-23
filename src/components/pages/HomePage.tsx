e297711086cf74e2e399ba1b01f8c068ed70e239
Wix Remote Machine <support@wix.com>
2026-05-23 22:52:12 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -282,7 +282,7 @@ export default function HomePage() {
                           Profile Overview
                         </span>
                         <p className="font-paragraph text-base md:text-lg text-foreground/80 leading-relaxed whitespace-pre-line">
-                          dangerouslySetInnerHTML={{__html: currentItem.description1 || ''}}
+                          {currentItem.description1 || ''}
                         </p>
                       </div>
                     </div>
